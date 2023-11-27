416eed973b7ef89043448b3c4eec4dc5e36ac568
rajath sinha <sinha.rajath0@gmail.com>
2023-11-27 12:23:35 +0530

    twwte

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,27 +1,36 @@
 const Task = require('../models/Task')
 
-const getAllTasks =(req,res)=>{
+const getAllTasks = (req, res) => {
     res.send('get all tasks')
 }
 
-const createTask=async(req,res)=>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+const createTask = async (req, res) => {
+
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+
+    }
+    catch(err)
+    {
+        res.status(500).json({msg:"errorrr!"})
+    }
+    
 }
 //test
 //damn it works
-const getTask=(req,res)=>{
-    res.json({id:req.params.id})
+const getTask = (req, res) => {
+    res.json({ id: req.params.id })
 }
 
-const updateTask=(req,res)=>{
+const updateTask = (req, res) => {
     res.send('update task')
 }
-const deleteTask=(req,res)=>{
+const deleteTask = (req, res) => {
     res.send('delete task')
 }
 
-module.exports ={
+module.exports = {
     getAllTasks,
     createTask,
     deleteTask,
